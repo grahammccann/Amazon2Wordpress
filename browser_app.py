67cffbdc2d935ef199a40e991f3ae8fdbaa41227
Graham McCann <graham23s@hotmail.com>
2023-09-02 10:35:27 +0100

    Updates.

--- browser_app.py
+++ browser_app.py
@@ -1,17 +1,25 @@
 import base64
 import json
 import os
+import openai
 import requests
-from PyQt5.QtWidgets import (QMainWindow, QLineEdit, QPushButton, QVBoxLayout,
+import markdown2
+from PyQt5.QtWidgets import (QMainWindow, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout,
                              QWidget, QTextEdit, QTabWidget, QDesktopWidget, QTableWidget, QTableWidgetItem,
                              QMessageBox, QComboBox, QLabel, QFormLayout)
+
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile, QWebEnginePage
 from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QCheckBox
+from dotenv import load_dotenv
 
+load_dotenv()
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 class BrowserApp(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.callback_counter = 0
         self.templates = ["Comparison Tables"]
         self.selected_template = self.templates[0]
         self.current_data = {}
@@ -25,11 +33,23 @@ class BrowserApp(QMainWindow):
         self.load_settings()
         self.browser.urlChanged.connect(self.update_url_textbox)
 
+    def generate_ai_product_data(self, product_name):
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "user", "content": f"Provide 5 bullet points about the product: {product_name}"}
+                ]
+            )
+            return markdown2.markdown(response.choices[0].message.content)
+        except openai.error.OpenAIError as e:
+            print(f"OpenAI API Error: {e}")
+            return None
+
     def initUI(self):
         self.setWindowTitle('Embedded Browser - graham23s@hotmail.com')
         self.setGeometry(100, 100, 800, 600)
         self.center()
-
         self.tabs = QTabWidget()
 
         # Browser Tab
@@ -75,9 +95,6 @@ class BrowserApp(QMainWindow):
         self.rendered_html_tab.setLayout(rendered_html_layout)
         self.tabs.addTab(self.rendered_html_tab, "Rendered HTML")
 
-        self.setCentralWidget(self.tabs)
-        self.tabs.currentChanged.connect(self.tab_changed)
-
         # Data Tab
         self.data_tab = QWidget()
         data_layout = QVBoxLayout()
@@ -103,8 +120,9 @@ class BrowserApp(QMainWindow):
         table_layout = QVBoxLayout()
 
         self.data_table = QTableWidget(self)
-        self.data_table.setColumnCount(5)
-        self.data_table.setHorizontalHeaderLabels(["Product Name", "Main Image", "Price", "Reviews", "Product URL"])
+        self.data_table.setColumnCount(6)
+        self.data_table.setHorizontalHeaderLabels(
+            ["Product Name", "Main Image", "Price", "Reviews", "Product URL", "Bullet Points"])
         table_layout.addWidget(self.data_table)
 
         self.table_tab.setLayout(table_layout)
@@ -127,9 +145,20 @@ class BrowserApp(QMainWindow):
         wp_html_layout.addWidget(self.wp_html_content)
 
         # Category Dropdown
-        self.category_dropdown = QComboBox(self)
         wp_html_layout.addWidget(QLabel("Select Category:"))
+        self.category_dropdown = QComboBox(self)
         wp_html_layout.addWidget(self.category_dropdown)
+
+        # Button Text Entry
+        wp_html_layout.addWidget(QLabel("Button Text:"))
+        self.button_text_entry = QLineEdit(self)
+        self.button_text_entry.setPlaceholderText('e.g. View on Amazon')
+        wp_html_layout.addWidget(self.button_text_entry)
+
+        # Show Price Checkbox
+        self.show_price_checkbox = QCheckBox("Show Price")
+        self.show_price_checkbox.setChecked(True)
+        wp_html_layout.addWidget(self.show_price_checkbox)
 
         # Post Button
         self.post_button = QPushButton('Post to WordPress', self)
@@ -143,24 +172,25 @@ class BrowserApp(QMainWindow):
         self.settings_tab = QWidget()
         settings_layout = QVBoxLayout()
 
+        # Use QFormLayout for the settings
         form_layout = QFormLayout()
 
-        self.wp_username_label = QLabel("WordPress Username:")
-        self.wp_username_entry = QLineEdit(self)
-        form_layout.addRow(self.wp_username_label, self.wp_username_entry)
+        self.amazon_domain_dropdown = QComboBox(self)
+        self.amazon_domain_dropdown.addItems(["amazon.co.uk", "amazon.com", "amazon.de"])
+        form_layout.addRow("Select Amazon Domain:", self.amazon_domain_dropdown)
 
-        self.wp_password_label = QLabel("WordPress Password:")
+        self.wp_username_entry = QLineEdit(self)
+        form_layout.addRow("WordPress Username:", self.wp_username_entry)
+
         self.wp_password_entry = QLineEdit(self)
         self.wp_password_entry.setEchoMode(QLineEdit.Password)
-        form_layout.addRow(self.wp_password_label, self.wp_password_entry)
+        form_layout.addRow("WordPress Password:", self.wp_password_entry)
 
-        self.wp_site_url_label = QLabel("WordPress Site URL:")
         self.wp_site_url_entry = QLineEdit(self)
-        form_layout.addRow(self.wp_site_url_label, self.wp_site_url_entry)
+        form_layout.addRow("WordPress Site URL:", self.wp_site_url_entry)
 
-        self.amazon_affiliate_id_label = QLabel("Amazon Affiliate ID:")
         self.amazon_affiliate_id_entry = QLineEdit(self)
-        form_layout.addRow(self.amazon_affiliate_id_label, self.amazon_affiliate_id_entry)
+        form_layout.addRow("Amazon Affiliate ID:", self.amazon_affiliate_id_entry)
 
         settings_layout.addLayout(form_layout)
 
@@ -200,8 +230,6 @@ class BrowserApp(QMainWindow):
             categories = response.json()
             for category in categories:
                 self.category_dropdown.addItem(category['name'], category['id'])
-        else:
-            QMessageBox.warning(self, "Error", "Failed to fetch categories from WordPress.")
 
     def load_settings(self):
         if os.path.exists(self.settings_file):
@@ -211,12 +239,14 @@ class BrowserApp(QMainWindow):
                 self.wp_password = data.get('wp_password', "")
                 self.wp_site_url = data.get('wp_site_url', "")
                 self.amazon_affiliate_id = data.get('amazon_affiliate_id', "")
+                self.amazon_domain = data.get('amazon_domain', "amazon.co.uk")  # Add this line
 
             # Set the text of the UI components
             self.wp_username_entry.setText(self.wp_username)
             self.wp_password_entry.setText(self.wp_password)
             self.wp_site_url_entry.setText(self.wp_site_url)
             self.amazon_affiliate_id_entry.setText(self.amazon_affiliate_id)
+            self.amazon_domain_dropdown.setCurrentText(self.amazon_domain)  # Add this line
 
             # Fetch categories after loading settings
             self.fetch_categories()
@@ -226,6 +256,7 @@ class BrowserApp(QMainWindow):
         self.wp_password = self.wp_password_entry.text()
         self.wp_site_url = self.wp_site_url_entry.text()
         self.amazon_affiliate_id = self.amazon_affiliate_id_entry.text()
+        self.amazon_domain = self.amazon_domain_dropdown.currentText()  # Add this line
 
         # Save to JSON file
         with open(self.settings_file, 'w') as file:
@@ -233,7 +264,8 @@ class BrowserApp(QMainWindow):
                 'wp_username': self.wp_username,
                 'wp_password': self.wp_password,
                 'wp_site_url': self.wp_site_url,
-                'amazon_affiliate_id': self.amazon_affiliate_id
+                'amazon_affiliate_id': self.amazon_affiliate_id,
+                'amazon_domain': self.amazon_domain  # Add this line
             }, file, indent=4)
 
         QMessageBox.information(self, "Settings Saved", "Your settings have been saved successfully!")
@@ -244,45 +276,13 @@ class BrowserApp(QMainWindow):
     def tab_changed(self, index):
         # Check if the current tab is "Rendered HTML"
         if self.tabs.tabText(index) == "Rendered HTML":
-            css_content = """
-            <style>
-                .a-comparison-table {
-                    width: 80%; /* Adjusted width */
-                    margin: 20px auto; /* Center the table */
-                    border-collapse: collapse;
-                    font-family: Arial, sans-serif;
-                    box-shadow: 0 0 20px rgba(0,0,0,0.15);
-                }
-                .a-comparison-table th, .a-comparison-table td {
-                    border: 1px solid #dddddd;
-                    padding: 12px 15px; /* Increased padding */
-                    text-align: center; /* Centered text */
-                }
-                .a-comparison-table th {
-                    background-color: #f7f7f7;
-                    color: #333;
-                    font-weight: bold;
-                }
-                .a-comparison-table tr:nth-child(even) {
-                    background-color: #f2f2f2;
-                }
-                .a-comparison-table tr:hover {
-                    background-color: #f5f5f5;
-                }
-                .a-comparison-table img {
-                    max-width: 100px; /* Adjust as needed */
-                    border-radius: 5px;
-                    box-shadow: 0 0 10px rgba(0,0,0,0.1);
-                }
-            </style>
-            """
-            html_content = css_content + self.wp_html_content.toPlainText()
+            bootstrap_cdn = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">'
+            html_content = bootstrap_cdn + self.wp_html_content.toPlainText().strip()  # Added .strip() here
             self.rendered_html_browser.setHtml(html_content)
 
     def render_html_content(self):
         bootstrap_cdn = '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">'
-        font_awesome_cdn = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">'
-        html_content = font_awesome_cdn + bootstrap_cdn + self.wp_html_content.toPlainText().strip()
+        html_content = bootstrap_cdn + self.wp_html_content.toPlainText().strip()  # Added .strip() here
         self.rendered_html_browser.setHtml(html_content)
 
     def center(self):
@@ -298,6 +298,11 @@ class BrowserApp(QMainWindow):
         self.browser.load(QUrl(url))
 
     def extract_data(self):
+
+        current_url = self.url_entry.text()
+        if "amazon.co.uk" not in current_url:
+            return  # Not an Amazon product page, so return without extracting data
+
         self.prompted = False  # Reset the prompted flag
 
         # Product Name
@@ -334,6 +339,8 @@ class BrowserApp(QMainWindow):
 
     def store_and_check_data(self, key):
         def callback(result):
+            self.callback_counter += 1
+
             if result:
                 self.current_data[key] = result
                 self.result_text.append(
@@ -343,9 +350,12 @@ class BrowserApp(QMainWindow):
 
             # Check if all data points are present
             required_data_points = ['product_name', 'main_image', 'price', 'reviews']
-            if all(point in self.current_data for point in required_data_points) and not self.prompted:
+            if self.callback_counter == len(required_data_points) and not self.prompted:
                 self.prompted = True
                 self.prompt_extraction()
+
+                # Reset the counter for the next extraction
+                self.callback_counter = 0
 
             # Add a newline after the last required data point is extracted
             if key == required_data_points[-1]:
@@ -361,6 +371,13 @@ class BrowserApp(QMainWindow):
         msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
         retval = msg.exec_()
         if retval == QMessageBox.Yes:
+            # Generate bullet points using AI
+            product_name = self.current_data['product_name']
+            product_info = self.generate_ai_product_data(product_name)
+            if product_info:
+                self.result_text.append(f"[INFO] AI Generated Product Info: {product_info}")
+                # Store the AI data in current_data
+                self.current_data['product_info'] = product_info
             self.add_to_table()
 
     def template_selected(self, index):
@@ -371,65 +388,123 @@ class BrowserApp(QMainWindow):
         for i in range(rows):
             if self.data_table.item(i, 0).text() == self.current_data['product_name']:
                 return  # Duplicate found, do not add
+
         self.data_table.insertRow(rows)
         self.data_table.setItem(rows, 0, QTableWidgetItem(self.current_data['product_name']))
         self.data_table.setItem(rows, 1, QTableWidgetItem(self.current_data['main_image']))
         self.data_table.setItem(rows, 2, QTableWidgetItem(self.current_data['price']))
         self.data_table.setItem(rows, 3, QTableWidgetItem(self.current_data['reviews']))
-        self.data_table.setItem(rows, 4, QTableWidgetItem(self.url_entry.text()))  # Add the product URL to the data table
+        self.data_table.setItem(rows, 4, QTableWidgetItem(self.url_entry.text()))
+
+        # Generate AI bullet points for the product
+        product_info = self.generate_ai_product_data(self.current_data['product_name'])
+        self.current_data['product_info'] = product_info  # Update the dictionary with the new bullet points
+        bullet_points_html = product_info.replace("\n", "<br>")
+        self.data_table.setItem(rows, 5, QTableWidgetItem(product_info))  # Store bullet points in the table
 
         # Construct WordPress HTML based on the entire data table
-        table_rows_html = ""
+        product_boxes_html = ""
         for i in range(self.data_table.rowCount()):
             product_name = self.data_table.item(i, 0).text()
             main_image = self.data_table.item(i, 1).text()
             price = self.data_table.item(i, 2).text()
             reviews = self.data_table.item(i, 3).text()
-            product_url = self.data_table.item(i, 4).text()  # Get the product URL from the data table
+            product_url = self.data_table.item(i, 4).text()
+            product_info = self.data_table.item(i, 5).text() if self.data_table.item(i, 5) else ''
 
-            # Construct the Amazon URL with the affiliate tag for each product
-            amazon_base_url = "https://www.amazon.co.uk"
-            product_path = "/".join(product_url.split("/")[3:6])  # Extracting the essential part of the URL
-            affiliate_tag = f"?tag={self.amazon_affiliate_id}"
-            amazon_url = amazon_base_url + "/" + product_path + "/" + affiliate_tag
+            amazon_base_url = f"https://{self.amazon_domain}"
+            product_path = "/".join(product_url.split("/")[3:-1])
+            amazon_url = f"{amazon_base_url}/{product_path}/?tag={self.amazon_affiliate_id}"
 
-            # Add a Bootstrap-styled link that looks like a button to the table with Font Awesome cart icon for each product
-            amazon_link = f'<a href="{amazon_url}" class="btn btn-success" target="_blank"><i class="fas fa-shopping-cart"></i> Check Price</a>'
+            button_text = self.button_text_entry.text() or "View on Amazon"
+            show_price = self.show_price_checkbox.isChecked()
+            price_html = f'<span class="product-price">{price}</span>' if show_price else ""
 
-            table_rows_html += f"""
-            <tr>
-                <td><img src="{main_image}" alt="{product_name}" class="img-thumbnail" width="100"></td>
-                <td>{product_name}</td>
-                <td>{price}</td>
-                <td>{reviews}</td>
-                <td class="text-end">
-                    <div class="mt-2">
-                        {amazon_link}
+            product_boxes_html += f"""
+                <div class="product-container">
+                    <img src="{main_image}" alt="{product_name}" class="product-image">
+                    <div class="product-details">
+                        <h2 class="product-name">{product_name}</h2>
+                        <div class="product-price-reviews">
+                            {price_html}
+                            <span class="product-reviews">{reviews}</span>
+                        </div>
+                        <ul class="product-bullet-points">{bullet_points_html}</ul>
+                        <a href="{amazon_url}" target="_blank" class="view-on-amazon"><i class="fas fa-shopping-cart"></i> {button_text}</a>
                     </div>
-                </td>
-            </tr>
+                </div>
             """
 
-        html_content = f"""
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Image</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Reviews</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {table_rows_html}
-                </tbody>
-            </table>
-        </div>
+        wp_html = f"""
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+            <style>
+                .product-container {{
+                    width: 80%; 
+                    max-width: 600px; 
+                    border: 1px solid #ddd;
+                    padding: 20px; 
+                    font-family: Arial, sans-serif;
+                    box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+                    margin: 20px auto; 
+                }}
+                .product-image {{
+                    width: 100%;
+                    height: auto;
+                    display: block;
+                    margin-bottom: 20px; 
+                }}
+                .product-name {{
+                    font-size: 20px; 
+                    font-weight: bold;
+                    margin-bottom: 15px; 
+                }}
+                .product-price-reviews {{
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                }}
+                .product-price {{
+                    color: #B12704;
+                    font-size: 18px; 
+                    margin-right: 10px; 
+                }}
+                .product-reviews {{
+                    font-size: 16px; 
+                }}
+                .product-bullet-points {{
+                    list-style-type: disc;
+                    padding-left: 30px; 
+                    font-size: 16px; 
+                    margin-bottom: 20px; 
+                }}
+                .view-on-amazon {{
+                    display: block;
+                    width: 100%;
+                    padding: 12px; 
+                    background-color: #FEBD69;
+                    text-align: center;
+                    color: white;
+                    text-decoration: none;
+                    border-radius: 4px;
+                    font-size: 18px; 
+                }}
+                .view-on-amazon:hover {{
+                    background-color: #f5a623;
+                }}
+                .view-on-amazon i {{
+                    margin-right: 8px; 
+                }}
+            </style>
+            {product_boxes_html}
         """
 
-        self.wp_html_content.setPlainText(html_content)
+        self.wp_html_content.setPlainText(wp_html)
 
-        # Reset the current_data dictionary to ensure it's empty for the next product
-        self.current_data = {}
+
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+    window = BrowserApp()
+    window.show()
+    sys.exit(app.exec_())
